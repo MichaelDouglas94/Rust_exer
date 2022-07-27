b65bd2c367a83153b0f588feb884075468500490
Michael Douglas <mikedouglasal94@gmail.com>
2022-07-26 21:45:47 -0300

    fix a bug

--- exer01/guessing_game/src/main.rs
+++ exer01/guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng; // método para gerar números aleatórios
 
 
@@ -18,13 +19,14 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
     
+    let guess: u32 = guess.trim().parse().expect("Please type a number");
+
     println!("You guesssed: {guess}");
 
+
     match guess.cmp(&secret_number) {
-        Ordering::less => println!("Too small!"),
-        Ordering::Greater => println("Too big"),
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big"),
         Ordering::Equal => println!("You win!!"),
     }
 }
-
-// arm’s pattern. tradução 
