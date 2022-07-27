ed43c44f7ee19d35bb32b3ab0b3b5ea3ec342522
Michael Douglas <mikedouglasal94@gmail.com>
2022-07-26 21:56:20 -0300

    add loop function

--- exer01/guessing_game/src/main.rs
+++ exer01/guessing_game/src/main.rs
@@ -8,25 +8,31 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    //println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess!");
+    loop {
+        println!("Please input your guess!");
 
-    // Comentários, o mut permite que a variável seja mutável. 
-    let mut guess = String::new();
+        // Comentários, o mut permite que a variável seja mutável. 
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        let guess: u32 = guess.trim().parse().expect("Please type a number");
 
-    println!("You guesssed: {guess}");
+        println!("You guesssed: {guess}");
 
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You win!!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!!");
+                break;
+            }
+
+        }
     }
 }
